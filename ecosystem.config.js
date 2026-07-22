50e2f0c3932991ef776baaaf76da3d84ff5aeda7
Amahseyn <mhhashemi1379@gmail.com>
2026-07-22 19:43:21 +0000

    fix: update CI/CD deploy paths and PM2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'frontend',
-      cwd: '/var/www/frontend',
+      cwd: '/home/Taxi/Taxi_frontend',
       script: 'npm',
       args: 'start -- --port 3000',
       interpreter: 'none'
